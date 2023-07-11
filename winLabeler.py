e5f71561cb27778191f5fdc094de6ad8e19aa069
fsabuhi <93775724+fsabuhi@users.noreply.github.com>
2023-07-11 17:17:35 +0400

    Added python argparse library to handle cli arguments

--- winLabeler.py
+++ winLabeler.py
@@ -2,9 +2,18 @@ from src.texts import *
 from src.image import *
 from src.tools import *
 
+import argparse 
+parser = argparse.ArgumentParser() # Create the parser
+parser.add_argument('--path', type=str, help="Path of the image you want to use as the label icon", required=True) # Add an argument
+parser.add_argument('size', type=str, help="Image Size. small or large")
+args = parser.parse_args() # Parse the argument. you can access to -path by args.path
+
+if args.size.lower() not in ["small","large"]: #check for size argument. must be "large" or "small"
+    parser.error('Size has to be "large" or "small"')
+
 welcomeString()
 
-file_path = input("> Enter the path of the icon label: ")
+file_path = args.path
 
 if not os.path.exists(file_path):
     print("    [!] File does not exist!")
@@ -20,9 +29,9 @@ prepareLabel(file_path)
 
 compositeLabel()
 
-small_folder = input("> Use small folder? (y/n): ")
+small_folder = args.size.lower()
 
-if small_folder.lower() in ["y", "yes"]:
+if small_folder.lower() == "small":
     s = True
 else:
     s = False
